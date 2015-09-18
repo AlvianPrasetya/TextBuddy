026c2aedb36165839f7d2c7dd35026efc65e534c
Alvian Prasetya <alvian.better@gmail.com>
2015-09-19 00:29:27 +0800

    Added spacings between import packages.

--- src/com/alvian/main/TextBuddy.java
+++ src/com/alvian/main/TextBuddy.java
@@ -1,10 +1,12 @@
 package com.alvian.main;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.io.IOException;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
